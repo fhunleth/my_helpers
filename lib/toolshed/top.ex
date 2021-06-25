150dd23ae951a7c243baa8dc43314f34c31dbcce
Frank Hunleth <fhunleth@troodon-software.com>
2021-06-25 09:23:00 -0400

    top: reorganize implementation

--- lib/toolshed/top.ex
+++ lib/toolshed/top.ex
@@ -2,14 +2,38 @@ defmodule Toolshed.Top do
   @moduledoc """
   Find the top processes
   """
+
+  @default_n 10
+
   @doc """
-  List the running  processes and auto-update them after every `1_000` milliseconds.Press enter to exit.
+  Interactively show the top Elixir processes
+
+  This is intended to be called from the IEx prompt and will periodically
+  update the console with the top processes. Press enter to exit.
+
+  Options:
+
+  * `:order` - the sort order for the results (`:reductions`, `:delta_reductions`,
+    `:mailbox`, `:delta_mailbox`, `:total_heap_size`, `:delta_total_heap_size`, `:heap_size`,
+    `:delta_heap_size`, `:stack_size`, `:delta_stack_size`)
+  * `:n`     - the max number of processes to list
   """
-  def top() do
+  @spec top(keyword()) :: :ok
+  def top(opts \\ []) do
+    options = process_options(opts)
+
     IO.puts("Press enter to stop\n")
 
-    {:ok, pid} = Toolshed.TopServer.start_link([])
+    {:ok, pid} = Toolshed.Top.Server.start_link(options)
     _ = IO.gets("")
-    Toolshed.TopServer.stop(pid)
+    Toolshed.Top.Server.stop(pid)
+  end
+
+  # TODO - validate options
+  defp process_options(opts) do
+    order = Keyword.get(opts, :order, :delta_reductions)
+    n = Keyword.get(opts, :n, @default_n)
+
+    %{order: order, n: n}
   end
 end
